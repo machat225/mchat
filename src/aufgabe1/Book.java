03f2e128262a02a5b7f5b965d189fee998ef8815
machat225 <mchatti@smail.uni-koeln.de>
2025-04-22 17:12:03 +0200

    1

--- src/aufgabe1/Book.java
+++ src/aufgabe1/Book.java
@@ -1,5 +1,15 @@
 package aufgabe1;
 
-public class Book {
-
+public class Book extends Publication{
+	private String author;
+	
+	public Book(String title, int year, String author) {
+		super(title, year);
+		this.author = author;
+	}
+	
+	@Override
+	public String getInfo() {
+		return super.getInfo() + ", by " + author;
+	}
 }
--- src/aufgabe1/Textbook.java
+++ src/aufgabe1/Textbook.java
@@ -1,5 +1,16 @@
 package aufgabe1;
 
-public class Textbook {
-
+public class Textbook extends Book{
+	private String subject;
+	
+	public Textbook(String title, int year, String author, String subject) {
+		super(title, year, author);
+		this.subject = subject;
+	}
+	
+	@Override
+	public String getInfo() {
+		return super.getInfo() + ", Subject: " + subject;
+		
+	}
 }
